296ba2ca81c3b6014411961580d276c745b0938f
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-11-02 12:01:35 -0300

    Criada as exceções

--- get.py
+++ get.py
@@ -2,7 +2,7 @@ from clipboard import paste
 
 lista_ctrl_c = []
 
-def get_ctrl() -> list:
+def get_ctrl() -> (list|str):
     global lista_ctrl_c
 
     excecoes = ['\n', '\t', '\r', "\'", '\"','\a', '\b','\\']
@@ -16,4 +16,8 @@ def get_ctrl() -> list:
     
     if not cola in lista_ctrl_c:
         lista_ctrl_c.append(cola)
+    
+    if lista_ctrl_c == None:
+        pass
+    else:
         return lista_ctrl_c
