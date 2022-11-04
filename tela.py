0a7cf5a055e636a88deafafb7dcbf3ae05d7583a
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-11-03 22:40:04 -0300

    Continuando código

--- tela.py
+++ tela.py
@@ -1,24 +1,29 @@
 import tkinter as tk
+import threading
+import get
 from keyboard import add_hotkey, wait
 
-key = True
-
 def show_window() -> None:
-    global key
+    def maximizar() -> None:
+       WINDOW.state(newstate='normal')
+    
+    add_hotkey('win + v', maximizar)
 
-    if key is True:
-        key = False
-        add_hotkey('win + v', minimizar)
-    else:
-        key = True
-        add_hotkey('win + v', maximizar)
-    wait()
+
+def hide_window() -> None:
+    WINDOW.state(newstate='iconic')
 
 
 def disable_event() -> None:
     pass
 
 
+def display() -> None:
+    card = tk.Text(WINDOW, height=10, width=30, fg='white')
+    card.insert('end', chars=get.get_ctrl_c())
+    card.pack()
+    
+    
 WINDOW = tk.Tk()
 WINDOW.resizable(width=False, height=False)
 WINDOW.title("Criptografador")
@@ -26,16 +31,12 @@ WINDOW.resizable(False, False)
 WINDOW.geometry('600x600')
 WINDOW.wm_attributes('-topmost', True)
 #WINDOW.protocol("WM_DELETE_WINDOW", disable_event)
+#WINDOW.state(newstate='iconic')
 WINDOW.wm_maxsize(600, 600)
 WINDOW['bg'] = "black"
 
-def maximizar() -> None:
-    WINDOW.state(newstate='normal')
+tk.Label(text=display())
+WINDOW.after(2, display)
 
-
-def minimizar() -> None:
-    WINDOW.state(newstate='iconic')
-
-
-show_window()
+threading.Thread(target=show_window).start()
 WINDOW.mainloop()
