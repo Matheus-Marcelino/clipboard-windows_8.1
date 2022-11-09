6595a744471061f4b7bdb4dae5d6d47ebc0fb8f6
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-11-08 21:03:04 -0300

    Partindo para aprte final da tela

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from get import get_ctrl_c
 from threading import Thread
 from keyboard import add_hotkey, wait
 
+ident = 0  # variavel de controle para a criação de card
+
 
 def config_window_state() -> None:
     def state() -> None:
@@ -16,64 +18,63 @@ def config_window_state() -> None:
     wait()
 
 
-def Card() -> None:
-    def create_Card() -> None:
+def Card_func() -> None:
+    global ident
+    
+    def create_Card() -> None: 
         linha = 0
+        for frase in copy:
         
-        for c in copy:
+            card = tk.Text(frame_text, width=40, height=6, autoseparators=True)
             card.grid(column=0, row=linha, padx=WIDTH/10, pady=3)
-            card.insert(index='end', chars=c)
+            card.insert(index='end', chars=frase)
+            frame_text.update_idletasks()
             linha += 1
-            WINDOW.update_idletasks()
-
+        canvas['scrollregion'] = (0, 0, frame_text.winfo_reqheight(), frame_text.winfo_reqheight())
+        
     print(get_ctrl_c())
     copy, tamanho = get_ctrl_c(), len(get_ctrl_c())
-    ident = 0
 
-    if tamanho != ident:
-        create_Card()
+    if ident != tamanho:
         ident += 1
-
-    WINDOW.after(5000, card)
+        create_Card()
+    
+    frame_text.after(5000, Card_func)
 
 
 def new_event_delete() -> None:
     WINDOW.state(newstate='iconic')
 
 
-#def move_window(event: any) -> None:
-#    WINDOW.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
-
 WINDOW = tk.Tk()
 WINDOW['bg'] = "#1e1e1e"
 WINDOW.title("Area de Transferência -- alpha")
-#WINDOW.resizable(width=False, height=False)
-WINDOW.protocol("WM_DELETE_WINDOW", new_event_delete)
+WINDOW.resizable(width=False, height=False)
 WINDOW.wm_attributes('-topmost' , True)
-#WINDOW.wm_attributes('-toolwindow' , True)
+WINDOW.wm_attributes('-toolwindow' , True)
+#WINDOW.protocol("WM_DELETE_WINDOW", new_event_delete)
 #WINDOW.state(newstate='iconic')
-#WINDOW.rowconfigure(0, weight=0)
 
-HEIGHT = 500
-WIDTH = 400
+HEIGHT = int(500)
+WIDTH = int(400)
 X = WINDOW.winfo_screenwidth() // 2 - (WIDTH + 2) // 2
 Y = WINDOW.winfo_screenheight() // 2 - HEIGHT // 2
 WINDOW.geometry(f'{WIDTH}x{HEIGHT}+{X}+{Y}')
 
-"""
-title_bar = tk.LabelFrame(WINDOW, bg='#1e1e1e', font='Arial 12', relief='raised',
-                          height=25, text='Area de Transferência',
-                          labelanchor='n')
-title_bar.pack(expand=0, fill='x')
-title_bar.bind('<B1-Motion>', move_window)
-"""
-card = tk.Text(WINDOW, width=40, height=6)
-Card()
+canvas = tk.Canvas(WINDOW, bg="blue")
+canvas.grid(row=0, column=0, sticky=('n', 'w', 'e', 's'))
 
-scrollvert = ttk.Scrollbar(WINDOW, orient='vertical')
-scrollvert.grid(column=1, row=0, sticky='NS')
-scrollvert.config(command=card.yview)
+scrool = ttk.Scrollbar(WINDOW, orient="vertical", command=canvas.yview)
+scrool.grid(row=0, column=1, sticky=('n', 's'))
+canvas.configure(yscrollcommand=scrool.set)
+
+frame_text = tk.Frame(canvas, bg="grey")
+canvas.create_window((0, 0), window=frame_text, anchor='nw', height=HEIGHT)
+
+WINDOW.grid_columnconfigure(0, weight=1)
+WINDOW.grid_rowconfigure(0, weight=1)
+
+Card_func()
 
 Thread(target=config_window_state).start()
-WINDOW.update()
 WINDOW.mainloop()
