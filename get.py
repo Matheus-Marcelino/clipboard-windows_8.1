b2b692e6eba781ad582bf18d90a1b81ff1766efb
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-11-11 15:10:53 -0300

    .

--- get.py
+++ get.py
@@ -5,20 +5,20 @@ from os import path
 from shutil import rmtree
 from clipboard import paste, copy
 
-lista_ctrl_c = []
+token_list_ctrl_c = ''
 
 def get_ctrl_c() -> (list | None):
-    global lista_ctrl_c
+    global token_list_ctrl_c
 
     ctrl_c = paste()
 
-    if not ctrl_c in lista_ctrl_c and ctrl_c != '':
-        lista_ctrl_c.append(ctrl_c)
+    if not ctrl_c in token_list_ctrl_c and ctrl_c != '':
+        token_list_ctrl_c.append(ctrl_c)
 
-    if lista_ctrl_c == None:
+    if token_list_ctrl_c == None:
         pass
     else:
-        return lista_ctrl_c
+        return token_list_ctrl_c
 
 
 def copy_ctrl_c(phrase: str) -> None:
@@ -26,9 +26,9 @@ def copy_ctrl_c(phrase: str) -> None:
 
 
 def clear_ctrl_c() -> None:
-    global lista_ctrl_c
+    global token_list_ctrl_c
 
-    lista_ctrl_c.clear()
+    token_list_ctrl_c.clear()
 
 
 def delete() -> None:
